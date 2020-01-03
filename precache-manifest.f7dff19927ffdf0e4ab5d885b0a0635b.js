c46d34c113fed8f995a1420e1e959917791ea846
hello_chenchen <welcomechenchen@gmail.com>
2020-01-03 23:01:54 +0800

    Updates

---
+++ precache-manifest.f7dff19927ffdf0e4ab5d885b0a0635b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83add8d105c3922d2a0dddc11c8e0ec9",
+    "url": "/real-time-gold-price/index.html"
+  },
+  {
+    "revision": "58fb8c90ad6f1cf72de5",
+    "url": "/real-time-gold-price/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "604f34f2f79e056c0546",
+    "url": "/real-time-gold-price/static/js/2.4198ab93.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/real-time-gold-price/static/js/2.4198ab93.chunk.js.LICENSE"
+  },
+  {
+    "revision": "58fb8c90ad6f1cf72de5",
+    "url": "/real-time-gold-price/static/js/main.32c9b81d.chunk.js"
+  },
+  {
+    "revision": "532092302aa50b1990ae",
+    "url": "/real-time-gold-price/static/js/runtime-main.f67661a3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/real-time-gold-price/static/media/logo.5d5d9eef.svg"
+  }
+]);
